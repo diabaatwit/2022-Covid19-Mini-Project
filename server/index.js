13130fc9d2b43a725b34b727d729d630115419ad
Joel-beep <jcolon@umass.edu>
2022-01-27 22:31:46 -0500

    Configure Mongoose to connect to mongoDB database

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const mongoose = require('mongoose')
+//Configure Mongoose to connect to mongoDB database
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
