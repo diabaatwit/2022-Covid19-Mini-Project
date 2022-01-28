be39472c244d238a78ddc27f52dad2e2c9893f72
Diab <diaba@wit.edu>
2022-01-28 08:05:00 -0500

    connecting to DB

--- server/index.js
+++ server/index.js
@@ -4,6 +4,19 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const mongoose = require('mongoose')
+
+const url = 'mongodb+srv://nocturnals:AeT3RFlq38I3BXWp@cluster0.fqifx.mongodb.net/examResultsDB?retryWrites=true&w=majority'
+
+mongoose.connect(url).then(e => {
+  console.log('connected');
+}).catch(e =>{
+  console.error('not connected', e.error);
+});
+
+
+
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
