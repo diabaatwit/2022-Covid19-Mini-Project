95442f22ce2fd72aaf25020dc4bd335230678bae
Ruben Ruiz <rubendruiz2@gmail.com>
2022-01-30 20:09:07 -0500

    Was telling me I had a pending commiut, so I did it

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const mongoose = require('mongoose')
-<<<<<<< HEAD
 
 const url = 'mongodb+srv://nocturnals:AeT3RFlq38I3BXWp@cluster0.fqifx.mongodb.net/examResultsDB?retryWrites=true&w=majority'
 
@@ -17,9 +16,6 @@ mongoose.connect(url).then(e => {
 
 
 
-=======
-//Configure Mongoose to connect to mongoDB database
->>>>>>> 9ca346fc7ded3e63a238af6f439685c832f44b10
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
