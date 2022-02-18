b67307f09b237e32054476136fd66e64b302f1e7
Ruben Ruiz <rubenruiz@Rubens-MBP.fios-router.home>
2022-02-18 12:27:57 -0500

    added some ability to grab info from the user, and save it to a state

--- client/src/components/examForm.js
+++ client/src/components/examForm.js
@@ -1,8 +1,9 @@
-import React, {Component, useState} from 'react';
+import React, {Component} from 'react';
 
+const Exam = require('../../models/exam')
 
 // 
-//Nine boxes for info
+//Five boxes for info
 //Then 3 buttons for add, update, delete
 
 //helpful URL: https://www.geeksforgeeks.org/how-to-connect-mongodb-with-reactjs/?ref=rp
@@ -13,24 +14,34 @@ class ExamForm extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            numHours: "",
-            xRayImageLink: "",
+            numHours: "17",
+            xRayImageLink: "https://www.tbf.org/-/media/tbforg/images/gifts/hack-diversity-logo.gif?h=226&w=250&la=en&hash=2695C4EC5433CE76C7F84BF3134C8501CA64AFBE",
             keyFindings: "",
             brixiaScores: "",
             patientID: "",
             count: 0
         }
+        this.handleOnSubmit =  this.handleOnSubmit.bind(this);
+    }
+
+    handleOnSubmit =  (e) => {
+        
     }
 
     render() {
         return(
             <div className ="form-inputs">
-                <button>Add</button>
-                <input placeholder='numHours?'></input>
-                <input placeholder='X-ray Image Link'></input>
-                <input placeholder='Key Findings'></input>
-                <input placeholder='Brixia Scores'></input>
-                <input placeholder='Patient ID'></input>
+                <form action="" onSubmit={this.handleOnSubmit()}>
+                    <input type="text" required={true} placeholder="Key Findings" 
+                    defaultValue={this.state.keyFindings} onChange={(e) => this.setState({ keyFindings: e.target.value }) } />
+                    <input type="text" required={true} placeholder="Brixia Scores" 
+                    defaultValue={this.state.brixiaScores} onChange={(e) => this.setState({ brixiaScores: e.target.value }) } />
+                    <input type="text" required={true} placeholder="X-ray Image Link" 
+                    defaultValue={this.state.xRayImageLink} onChange={(e) => this.setState({ xRayImageLink: e.target.value }) }>
+                    </input>
+                    <button type="submit"
+                    value="submit">submit</button>
+                </form>
                 <button>Delete</button>
                 <button>Update</button>
                 <button onClick={() => this.setState({ count: this.state.count+1 })}> {this.state.count} </button>
