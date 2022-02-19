525e52c1900b680af8188a94aca492930bf10916
Diana Ruiz-Olvera <dp.ruizolvera@gmail.com>
2022-02-19 12:11:59 -0500

    Exam Page/Card Front-End work

--- client/src/components/examInfo.js
+++ client/src/components/examInfo.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import Table from './TableExams'
+import { Link } from 'react-router-dom';
+import './table.css'
 
 class ExamInfo extends Component {
 
@@ -10,10 +11,8 @@ class ExamInfo extends Component {
           isLoading: false,
           isError: false,         
         }
-        
+
       }
-
-
       
     /* fetching exam */
     async componentDidMount() {
@@ -31,38 +30,39 @@ class ExamInfo extends Component {
         } else {
             this.setState({ isError: true, isLoading: false })
             //console.log(error)
-
         }
 
     }
 
-
-
+    /* render exam data*/     
     examData = () => {
-        
         return this.state.exams.map(exam => {
-            
             return (
-                
                 <div key={exam._id}>
+                    <div className="spacer"/>
                     <div>{exam._id}</div>
+                    <div className="spacer"/>
                     <div>{exam.numHours}</div>
-                    <div><img src={exam.xRayImageLink} alt="xRayImage"/></div>
-                    <div>{exam.keyFindings}</div>
-                    <div>{exam.brixiaScores}</div>
-                    <div>{exam.patientID}</div>
-                </div>
+                    <div className="spacer"/>
 
+                    <div>{exam.keyFindings}</div>
+                    <div className="spacer"/>
+                    <div className="card-item brixia-scores">{exam.brixiaScores}</div>
+                    <div className="spacer"/>
+                    <div>{exam.patientID}</div>
+
+                    <div><img src={exam.xRayImageLink} alt="xRayImage"/></div>
+                    <div className="spacer"/>
+
+                </div>
             )
         })
     }
 
-    
-
-
     render() {
         const { exams, isLoading, isError } = this.state
         console.log(exams.length)
+        
         //const [searchTerm, setSearchTerm] = useState('')
     
         if (isLoading) {
@@ -75,10 +75,76 @@ class ExamInfo extends Component {
         }
     
         return  exams.length > 0 ? (
-            //input tracking searchTerms
-            <div>
-              {this.examData()}
-    
+
+            <div class="card-container">
+              <div id="exampage-container"> 
+                <p id="exampage-label"> Exam Details</p>
+              </div>
+
+              <div class="card-title-container">
+                    <div>
+                      <img src={require('../images/exam-icon.png')} alt="Exam Icon" />
+                    </div>
+                    <div>
+                      <p>Exam</p>
+                    </div>
+              </div>
+
+              <div class="card-data-container">
+                <div class='row'>
+                  <div class='column'>
+                    <div class='title-column'>
+                      Exam ID
+                    </div><br/>
+                    <div class='title-column'>
+                      Date
+                    </div>
+                    <div class='title-column'>
+                      Brixia Scores
+                    </div>
+                    <div class='title-column'>
+                      Key Findings
+                    </div>
+                  </div>
+                <div class='column'>
+                  <div class='data-column'>
+                    {this.state.exams[0]._id}
+                  </div>
+                  <div class='data-column'>
+                    Feb-19-2022 (placeholder)
+                  </div>
+                  <div class='data-column'>
+                  {this.state.exams[0].brixiaScores}
+                  </div>
+                  <div class='data-column'>
+                  {this.state.exams[0].keyFindings}
+                  </div>
+                </div>
+              </div>  
+              </div>
+
+              {/*               
+              <div id="exampage-container">
+                <p id="exampage-label"> Exam ID:  {this.state.exams[0]._id}</p>
+              </div> */}
+          
+              {/* <div className = "exampage-details">
+                <div> Patient ID: {this.state.exams[0].patientID}</div>
+                <div > Age: {this.state.exams[0].patientID}</div>     
+                <div id="sex"> Sex: </div>
+                <div id="bmi"> BMI: </div>
+                <div id="zipcode"> Zip Code: </div>
+
+                <div id="brixia-header">Brixia Scores: {this.state.exams[0].brixiaScores}</div>
+                <div id="key-header"> Key Findings:</div>
+                <div id="key-findings"> {this.state.exams[0].keyFindings}</div>
+              </div> */}
+            
+              <div class="exampage-xraycard">
+                <img class="xray-image" src={this.state.exams[0].xRayImageLink} alt="xRayImage"/>
+                <div id="examimage-link"><Link to={this.state.exams[0].xRayImageLink} target="_blank" rel="noopener noreferrer"><span>View Full Image</span></Link></div>
+              </div>
+   
             </div>
           ) : (
             <div>No exams</div>
